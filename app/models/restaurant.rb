62f6def29dc313893bc488f08e341e05441662c6
Nona Schneider <nonaschneider99@gmail.com>
2021-03-03 16:39:13 +0100

    rails

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORY = %w[chinese italian japanese french belgian].freeze
+
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # get 'restaurants/name:string'
+  # get 'restaurants/address:text'
+  # get 'restaurants/category:string'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
+
+  # get "restaurants", to: "restaurants#index"
+
+  # get "reviews/new", to: "reviews#new"
+  # get "restaurants/new", to: "restaurantss#new"
+
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # post "reviews", to: "reviews#create"
+  # post "restaurants", to: "restaurants#create"
+
+  # resources :restaurants
+
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # delete "restaurants/:id", to: "restaurants#destroy"
 end
